438b02283610ac9fc8ef581da1bc7dcf78129fcb
Logan <15618309240@163.com>
2015-12-10 16:27:47 +0800

    :1 unexected end of input

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Login</title>
     <link type="text/css" rel="stylesheet" href="css/login.css" />
 </head>
