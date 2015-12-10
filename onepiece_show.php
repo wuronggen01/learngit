73121546569564c814fa3f2f0dfb5bc62a515397
Logan <15618309240@163.com>
2015-12-10 11:41:45 +0800

    wrote a file onepiece_show.php

---
+++ onepiece_show.php
@@ -0,0 +1,12 @@
+<DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<link type="text/css" rel="stylesheet" href="css/onepiece_show.css" />
+	<title></title>
+</head>
+<body>
+	<script type="text/javascript" src="js/jquery-1.11.3.js"></script>
+	<script type="text/javascript" src="js/onepiece_show.js" ></script>
+</body>
+</html>
